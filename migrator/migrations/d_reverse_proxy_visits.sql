594ae91fb56d3a86cba4e9c0313de94481dc1690
Jackson Stone <jacksonastone@gmail.com>
2024-07-13 09:03:45 -0500

    add

---
+++ migrator/migrations/d_reverse_proxy_visits.sql
@@ -0,0 +1,6 @@
+CREATE TABLE reverse_proxy_visits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    vistor_hash TEXT NOT NULL,
+    url_without_params TEXT NOT NULL,
+);
